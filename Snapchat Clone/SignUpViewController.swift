8cc000a30dbb75cf1f8b613e4698b291f08ad737
Cleber Junior <CleberJr@Clebers-MacBook-Pro.local>
2018-09-04 20:24:48 -0300

    signIn and SignUp pages

---
+++ Snapchat Clone/SignUpViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignUpViewController.swift
+//  Snapchat Clone
+//
+//  Created by Cleber Junior on 04/09/18.
+//  Copyright © 2018 Cleber Junior. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
