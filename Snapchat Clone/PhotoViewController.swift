2f6e0191544107c90e7596ccb882006a8ce99fa7
Cleber Junior <CleberJr@Clebers-MacBook-Pro.local>
2018-09-05 22:34:23 -0300

    select and take photo and send to firebase

--- Snapchat Clone/PhotoViewController.swift
+++ Snapchat Clone/PhotoViewController.swift
@@ -7,13 +7,77 @@
 //
 
 import UIKit
+import FirebaseStorage
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var imageDescription: UITextField!
+    @IBOutlet weak var text: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    var imagePicker = UIImagePickerController()
+    
+    
+    @IBAction func photoLibrary(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    
+        let selectedImage = info[ UIImagePickerControllerOriginalImage ] as! UIImage
+        
+        image.image = selectedImage
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        self.text.text = nil
+        
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        
+        self.nextButton.isEnabled = false
+        self.nextButton.setTitle("Loading...", for: .normal)
+        
+        let storage = Storage.storage().reference()
+        let images = storage.child("images")
+        
+        //recover image
+        if let selectedImage = image.image {
+            if let dataImage = UIImageJPEGRepresentation(selectedImage, 1) {
+                
+                images.child("image.jpg").putData(dataImage, metadata: nil, completion: { (metadata, erro) in
+                    
+                    if erro == nil {
+                        print("Success")
+                        self.nextButton.isEnabled = true
+                        self.nextButton.setTitle("Next", for: .normal)
 
+                    } else {
+                        print((erro?.localizedDescription)!)
+                    }
+                })
+                
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        takePhoto("photo")
+        
+
     }
 
     override func didReceiveMemoryWarning() {
